865d6d4bd8e2c052a31ac99699d6de25e114208f
EKthor <richardfc83@gmail.com>
2021-09-15 00:45:46 -0500

    Fin Seccion 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require( 'bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 const login  = async( req, res = response) => {
     
@@ -36,7 +37,8 @@ try {
 
     res.json({
         ok: true,
-        token
+        token,
+        menu: getMenuFrontEnd(usuarioDB.role)
     })
 
 } catch (error) {
@@ -52,7 +54,6 @@ try {
 
 
 const googleSignIn  = async( req, res = response) => {
-    
 
     const  googleToken = req.body.token;
     
@@ -84,7 +85,8 @@ const googleSignIn  = async( req, res = response) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd(usuario.role)
             /*msg : 'Google SignIn',
             name, email, picture*/
         })
@@ -113,7 +115,8 @@ const renewToken = async (req, res = response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd(usuario.role)
     })
 
 }
